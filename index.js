7e6295a8f9bce81e37c9fcc6658d39cd4e715b77
UniversalXeno <73885838+coderXeno@users.noreply.github.com>
2021-02-10 11:24:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,64 @@
-let value = document.getElementById('price-value');
-let views = document.getElementById('number-pageviews');
-let dollars = document.getElementById('number-price');
+const rangeBar = document.getElementsByTagName("input")[0];
+const views = document.getElementById("views");
+const billing = document.getElementById("monthlyPrice");
+const billing2 = document.getElementById("monthlyPrice2");
+const toggle = document.getElementById("toggle");
 
-let no_of_views = [10, 50, 100, 500, 1]
 
-let discount = document.getElementById('checkbox-discount');
-let determine = function(){
-    no_of_views = [10, 50, 100, 500, 1];
-    
-    //Discount Calculator
-    if(discount.checked){
-        for(var i = 0; i < no_of_views.length; i++){
-            no_of_views[i] -= no_of_views[i] * 0.25;
-        }
-    }
-    /* Listing out the values to be displayed when the slider
-    moved accordingly */
-    switch(slidebar.value){
-        case('20'):
-            pageviews.innerText = no_of_views[0]+'K';
-            price.innerText = "$8.00";
-            break;
-
-        case('40'):
-            pageviews.innerText = no_of_views[1]+'K';
-            price.innerText = "$12.00";
-            break;
-
-        case('60'):
-            pageviews.innerText = no_of_views[2]+'K';
-            price.innerText = "$16.00";
-            break;
-
-        case('80'):
-            pageviews.innerText = no_of_views[3]+'K';
-            price.innerText = "$24.00";
-            break;
-
-        case('100'):
-            pageviews.innerText = no_of_views[4]+'M';
-            if(discount.checked){
-                pageviews.innerText = "750K";
-            }
-            price.innerText = "$36.00";
-            break;
-    }
+yearDiscount = () => {
+let monthlyBillingp = Number(billing.textContent);
+let monthlyBillingp2 = Number(billing2.textContent);
+if (toggle.checked == true) {
+  billing.textContent = monthlyBillingp*=(75/100);
+  billing2.textContent = monthlyBillingp2*=(75/100);
+} else {
+  billing.textContent = monthlyBillingp*=(100/75);
+  billing2.textContent = monthlyBillingp2*=(100/75);
+}
 }
 
-determine();
+changePrice = () => {
+  let rangeValue = rangeBar.value;
+    switch(rangeValue){
+      case "20":
+        views.textContent = "10K PAGEVIEWS";
+        billing.textContent = "8";
+        billing2.textContent = "8";
+        rangeBar.style = "background: linear-gradient(90deg, hsl(174, 77%, 80%) 0%, hsl(224, 65%, 95%) 0%)"
+        break
 
-/* Adding Event listeners to slidebar and discount checkbox*/
-slidebar.addEventListener('mousemove', function(){
-    determine();
-});
+      case "40":
+        views.textContent = "50K PAGEVIEWS";
+        billing.textContent = "12";
+        billing2.textContent = "12";
+        rangeBar.style = "background: linear-gradient(90deg, hsl(174, 77%, 80%) 25%, hsl(224, 65%, 95%) 25%)"
+        break
 
-slidebar.addEventListener('touchmove', function(){
-    determine();
-});
+      case "60":
+        views.textContent = "100K PAGEVIEWS";
+        billing.textContent = "16";
+        billing2.textContent = "16";
+        rangeBar.style = "background: linear-gradient(90deg, hsl(174, 77%, 80%) 50%, hsl(224, 65%, 95%) 50%)"
+        break
 
-slidebar.addEventListener('change', function(){
-    determine();
-});
+      case "80":
+        views.textContent = "500K PAGEVIEWS";
+        billing.textContent = "24";
+        billing2.textContent = "24";
+        rangeBar.style = "background: linear-gradient(90deg, hsl(174, 77%, 80%) 75%, hsl(224, 65%, 95%) 75%)"
+        break
 
-discount.addEventListener('click', function(){
-    determine();
-});
+      case "100":
+        views.textContent = "1M PAGEVIEWS";
+        billing.textContent = "36";
+        billing2.textContent = "36";
+        rangeBar.style = "background: linear-gradient(90deg, hsl(174, 77%, 80%) 100%, hsl(224, 65%, 95%) 100%)"
+        break
+    }
 
-if(window.innerWidth < 480){
-    let pcnt_discount = document.getElementById('discount');
-    pcnt_discount.innerText = '-25%';
+    toggle.checked = false;
 }
+
+
+rangeBar.addEventListener("input", changePrice)
+toggle.addEventListener("click", yearDiscount)
